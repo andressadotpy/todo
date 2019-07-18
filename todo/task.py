f6a7d5bb8e17eaf029d523600e8904943cefb0ab
andressadotpy <acabistani@gmail.com>
2019-07-18 11:24:01 -0300

    Documenting Task

--- todo/task.py
+++ todo/task.py
@@ -3,9 +3,17 @@ from datetime import datetime
 from todo.note import Note
 
 class Task:
+    """Represents one task."""
     id = 1
 
     def __init__(self, name, note=None, due_date=None):
+        """Initializes each Task with a task name,
+        a date time of when the Task was created,
+        a status attribute primarly setted to 'unfinished',
+        a Note object (if the user wants to add a message to the task),
+        a due date (if the user wants to set a due date to the task),
+        and a unique id.
+        """
         self.name = name
         self.creation_date = datetime.today()
         self.status = 'unfinished'
@@ -15,16 +23,27 @@ class Task:
         Task.id += 1
 
     def get_task_name(self):
+        """Returns the string name of a Task."""
         return f'{self.name}'
 
     def get_note(self):
+        """Returns the string representation of the message attribute."""
         return f'{self.message}'
 
     def change_note(self, new_message):
+        """Creates another Note object and changes the Note object
+        associated with the message attribute of the Task.
+        Returns the string representation of the note.
+        """
         self.message = Note(new_message)
         return f'{self.message.note}'
 
     def change_status(self, new_status):
+        """Receives a new status argument and checks if the status
+        is valid. If it's valid, change the status attribute ot the Task
+        to the new status.
+        Raises NotValidStatus otherwise.
+        """
         valid_status = ['unfinished',
                         'in process',
                         'finished']
@@ -34,6 +53,9 @@ class Task:
         return self.status
 
     def match(self, word):
+        """Checks if a word matches the name attribute of the Task.
+        Returns True if matches and False otherwise.
+        """
         if word in self.name:
             return True
         return False
